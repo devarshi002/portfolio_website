cb7e4a0efe33aa272688a4661776ecfe09b54433
Devarshi Tambulkar <devarshitambulkar2@gmail.com>
2025-05-01 21:17:40 +0530

    first comment

--- script.js
+++ script.js
@@ -149,7 +149,7 @@ var hireMeLink = document.getElementById('hireMeLink');
 downloadLink.addEventListener('click', function(event) {
     event.preventDefault(); // Prevent default behavior of the anchor tag
     // Replace 'your_resume.pdf' with the actual name of your resume file
-    var resumeUrl = 'RESUME_deva.pdf';
+    var resumeUrl = 'software_engnieer_devarshi_tambulkar.pdf';
     // Create an anchor element
     var downloadLinkElement = document.createElement('a');
     // Set the download link's href attribute to the URL of your resume file
